df5fe4acab4d29c21763247917d3ec4b9f0f09aa
ademii27 <ademizunusbekkizi@gmail.com>
2026-03-25 09:19:11 +0500

    2

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-# Step a) Input & Variables
-driver_name = input("Enter driver name: ")
+name = input("Enter driver name: ")
+destination = input("Enter destination: ")
 distance = float(input("Enter distance (km): "))
-consumption = float(input("Enter fuel consumption (L/100km): "))
-fuel_price = float(input("Enter fuel price (KZT/L): "))
+consumption = float(input("Enter fuel consumption: "))
+price = float(input("Enter fuel price: "))
 
-# Step b) Calculations
-litres_needed = distance * consumption / 100
-fuel_cost = litres_needed * fuel_price
-cost_per_km = fuel_cost / distance
+fuel_cost = (distance / 100) * consumption * price
 
-# Step c) Formatted Output
-print("=" * 30)
-print("ROAD TRIP SUMMARY")
-print("=" * 30)
-print("Driver : ", driver_name)
-print("Distance : ", distance, " km")
-print("Consumption : ", consumption, "L/100km")
-print("Fuel price : ", fuel_price, "KZT/L")
-print("-" * 30)
-print("Litres needed: ", litres_needed, "L")
-print("Fuel cost ", fuel_cost, "KZT")
-print(f"Cost per km ", cost_per_km, "KZT")
-print("=" * 30)
+# Category
+if distance < 100:
+    category = "Short trip"
+elif distance < 500:
+    category = "Medium trip"
+else:
+    category = "Long trip"
 
-# Step d) Comparison
-print("Trip longer than 300 km:", distance > 300)
-print("Fuel cost above 5000 KZT:", fuel_cost > 5000)
+print("Driver :", name)
+print("Destination :", destination.upper())
+print("Distance :", distance, "km")
+print("Fuel cost :", fuel_cost,"KZT")
+print("Category :", category)
+
+# Cost breakdown
+print("Cost breakdown:")
+for i in range(100, int(distance)+1, 100):
+    cost = (i / 100) * consumption * price
+    print(i, "km →", cost)
+
+# Destination analysis
+print("Destination Uppercase:", destination.upper())
+print("Destination lowercase:", destination.lower())
+print("Length:", len(destination))
+print("Letter a:", destination.lower().count("a"))
+
